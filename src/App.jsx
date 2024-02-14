3cd348df65a4e98a17294864c762aff4d4bc2521
Success Obasi <obasisuccess192@gmail.com>
2024-02-14 15:13:09 +0100

    all good

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import { FaBars } from 'react-icons/fa';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-col font-poppins'>
+      <header className='flex justify-between p-4 items-center'>
+        <img src="/images/WCF-UNN-logo.png" alt="" className='w-8'/>
+        <FaBars className='text-xl' /> 
+      </header>
+      <main>
+        <section className='hero-img  bg-bg-mobile bg-no-repeat relative h-[297px] bg-cover bg-center'> 
+          <div className='absolute overlay bg-transparent-blue w-full flex flex-col items-center justify-center  top-0 left-0 h-[297px]'>
+            <p className='text-center stroke-white text-[24px] text-white w-[262px] font-semibold pb-6'>
+            "God is spirit, and those who worship him must worship in spirit and truth."
+            </p>
+            <p className="font-zhi-mang-xing text-center text-white text-[32px] ">John 4:24</p>
+          </div>
+        </section>
+      </main>
+    </div>
+
   )
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': [ 'Poppins', 'sans-serif'],
+        'zhi-mang-xing': [ 'Zhi Mang Xing', 'cursive'],
+      },
+      backgroundImage: {
+        'bg-mobile': "url('/images/img-1.png')"
+      },
+      
+      letterSpacing: {
+        widest: "0.5em",
+      },
+      colors: {
+        'transparent-blue': "rgba(24, 53, 109, 0.63)",
+        'primary-blue': "#0E3887",
+        'event-blue': "#01032E",
+        'gallery-blue': "#16034D",
+        'active-blue': "#0DCBD7",
+
+      },
+    },
   },
   plugins: [],
 }
